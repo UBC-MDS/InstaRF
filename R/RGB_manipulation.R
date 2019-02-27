c7a6178b3ddc461c482c7b0d62bdc0d1d8756d45
Betty Zhou <betty.zhou@alumni.ubc.ca>
2019-02-26 19:27:53 -0800

    fixed test errors

--- R/RGB_manipulation.R
+++ R/RGB_manipulation.R
@@ -15,46 +15,46 @@ library(png)
 library(testit)
 
 RGB_manipulation <- function(input_path, output_path, R = 2L, G = 2L, B = 2L){
-  
+
   # exceptional handling
-  assert("Please provide the input path as a string", is.character(input_path))
-  assert("Please provide the output path as a string", is.character(output_path))
-  assert("Please provide red channel weight as integer", is.integer(R))
-  assert("Please provide green channel weight as integer", is.integer(G))
-  assert("Please provide blue channel weight as integer", is.integer(B))
-  
+  testit::assert("Please provide the input path as a string", is.character(input_path))
+  testit::assert("Please provide the output path as a string", is.character(output_path))
+  testit::assert("Please provide red channel weight as integer", is.integer(R))
+  testit::assert("Please provide green channel weight as integer", is.integer(G))
+  testit::assert("Please provide blue channel weight as integer", is.integer(B))
+
   # read in image from input path
-  img <- readPNG(input_path) * 255
-  
+  img <- png::readPNG(input_path) * 255
+
   # extract height and weight of input image
   h <- dim(img)[1]
   w <- dim(img)[2]
-  
+
   # Separate out the RGB channels into individual matrices
   R_mat <- img[, , 1]
   G_mat <- img[, , 2]
   B_mat <- img[, , 3]
-  
+
   # adjust the intensities for each channel based on weight arguments
   new_R <- R_mat * R
   new_G <- G_mat * G
   new_B <- B_mat * B
-  
+
   # Correct RGB intensities to be within 0 to 255
   maxV <- 255
   minV <- 0
   RGB_bound <- c()
-  
+
   for (i in list(new_R, new_G, new_B)){
     bound <- apply(i, c(1,2), function(x) min(max(x, minV), maxV))
     RGB_bound <- c(RGB_bound, bound)
   }
-  
+
   output <- array(RGB_bound,
                   dim = c(h,w,3))
-  
+
   # save RGB manipulated image to output path
-  writePNG(output, output_path)
+  png::writePNG(output/255, output_path)
 }
 
 
